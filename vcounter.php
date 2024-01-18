c67802195a4767266d4269a33e23760d39381db1
Saruman212 <156678308+saruman212@users.noreply.github.com>
2024-01-18 07:32:57 -0600

    Create vcounter.php

---
+++ vcounter.php
@@ -0,0 +1,17 @@
+<?php
+function contadorvisitas () {
+  $archivo = "contadorvisitas.txt"; //el archivo de texto que contendra las visitas
+  $f = fopen ($archivo, "r"); //abrimos el fichero en modo de lectura
+  if ($f) {
+    $contadorvisitas = fread ($f, filesize ($archivo)); //vemos el archivo de texto
+    $contadorvisitas = $contadorvisitas + 1; //Le sumamos +1 al contador de visitas
+    fclose ($f);
+  }
+  $f = fopen ($archivo, "w+");
+  if ($f) {
+    fwrite ($f, $contadorvisitas);
+    fclose ($f);
+  }
+  return $contadorvisitas;
+}
+?>
